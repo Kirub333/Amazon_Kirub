93c6c17727202a3426d8bdef8e0480444d715b40
Kirubel Ayele <kirub333@gmail.com>
2024-11-21 11:01:15 +0300

    Products

--- amazon/src/components/Product/Product.jsx
+++ amazon/src/components/Product/Product.jsx
@@ -2,46 +2,32 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import classes from "./Product.module.css";
-import Loader from "../Loader/Loder";
 import ProductCard from "./ProductCard";
-import { FakeStoreAPI_BaseURL } from "../../API/EndPoints";
 
 function Product() {
-  const [products, setProducts] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setIsLoading(true);
     axios
-      .get(`${FakeStoreAPI_BaseURL}/products`)
+      .get("https://fakestoreapi.com/products")
       .then((res) => {
         setProducts(res.data);
-        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
-        setIsLoading(false);
       });
   }, []);
 
   return (
-    <>
-      {isLoading ? (
-        <Loader />
+    <section className={classes.products_container}>
+      {products.length > 0 ? (
+        products.map((singleProduct) => (
+          <ProductCard product={singleProduct} key={singleProduct.id} />
+        ))
       ) : (
-        <section className={classes.products_container}>
-          {products?.map((singleProduct) => {
-            return (
-              <ProductCard
-                renderAdd={true}
-                product={singleProduct}
-                key={singleProduct.id}
-              />
-            );
-          })}
-        </section>
+        <p>Loading products...</p> // Display a loading message while products are being fetched
       )}
-    </>
+    </section>
   );
 }
 
--- amazon/src/components/Product/ProductCard.jsx
+++ amazon/src/components/Product/ProductCard.jsx
@@ -1,83 +1,30 @@
-import React, { useContext } from "react";
+import React from "react";
 import Rating from "@mui/material/Rating";
 import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
-import styles from "./Product.module.css";
-import { Link } from "react-router-dom";
-import { Type } from "../../Utility/action.type";
-import { DataContext } from "../../components/DataProvider/DataProvider";
-import { BsFillCartXFill } from "react-icons/bs";
+import classes from "./Product.module.css";
 
-function ProductCard({ product, flex, renderDesc, renderAdd , showRemoveItem, itemAmount,total }) {
+function ProductCard({ product }) {
   const { image, title, id, rating, price, description } = product;
 
-  const [state, dispatch] = useContext(DataContext);
-  
-// console.log(state)
-
-  const addToCart = () => {
-    dispatch({
-      type: Type.ADD_TO_BASKET,
-      item: {
-        image,
-        title,
-        id,
-        rating,
-        price,
-        description,
-      },
-    });
-  };
-
   return (
-    <div
-
-      className={`${styles.card__container} ${
-        flex ? styles.product__flexed : ""
-      }`}
-    >
-      <Link to={`/products/${id}`}>
-        <img src={image} alt="" className={styles.img_container} />
-      </Link>
+    <div className={`${classes.card__container}`}>
+      <a href="">
+        <img src={image} alt="" className={classes.img_container} />
+      </a>
       <div>
         <h3>{title}</h3>
-        {renderDesc && <div style={{ maxWidth: "750px" }}>{description}</div>}
-        <div className={styles.rating}>
+        <div className={classes.rating}>
           {/* rating */}
-          <Rating value={rating?.rate} precision={0.1} />
+          <Rating value={rating.rate} precision={0.1} />
           {/*count  */}
-          <small>{rating?.count}</small>
+          <small>{rating.count}</small>
         </div>
-        <div  style={{ display: "flex", justifyContent: "space-between", alignItems: "center",}}>
-        {/* <div className={styles.price_border}> */}
-            {/* price */}
-            <CurrencyFormat amount={price}  /> 
-            {itemAmount && <p style={{fontWeight:"500",color:"var(--primary-color)"}}>Quantity: {itemAmount}</p>}
-           {total && <p style={{fontWeight:"500",color:"var(--primary-color)"}}>Total: ${price*itemAmount}</p>}
-        
-        {/* </div> */}
         <div>
-        {
-          showRemoveItem && (
-            <button   
-           className={styles.button}
-            style={
-              {display:"flex", justifyContent:"center",alignItems:"center" ,gap:"7px",padding:"5px"}
-            }
+          {/* price */}
+          <CurrencyFormat amount={price} />
+        </div>
 
-              onClick={() => dispatch({ type: Type.REMOVE_ITEM_IMMEDIATELY, id })}
-          ><BsFillCartXFill size={20}/>
-            Remove Item
-               
-            </button>
-          )
-        }
-        </div>
-        </div>
-        {renderAdd && (
-          <button className={styles.button} onClick={addToCart}>
-            add to cart
-          </button>
-        )}
+        <button className={classes.button}></button>
       </div>
     </div>
   );
