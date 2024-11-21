186e7679f80741026dc4a20eb7860569a4447a23
Kirubel Ayele <kirub333@gmail.com>
2024-11-21 09:20:02 +0300

    Catagory

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import CarouselEffect from "./components/Carousel/CarouselSlider";
+import Category from "./components/Category/Category";
 
 function App() {
   return (
     <div>
       <Header />
       <CarouselEffect />
+      <Category />
     </div>
   );
 }
