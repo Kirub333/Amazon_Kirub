ab6f4ed4c9fb4f5f24d61c126744a741c487bc59
Kirubel Ayele <kirub333@gmail.com>
2024-11-26 19:56:39 +0300

    payment 1

--- amazon/src/Router.jsx
+++ amazon/src/Router.jsx
@@ -7,6 +7,14 @@ import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51QPVXNJkKq2JimUd8Sp65ZdhaQNpc3axR9XKAhDLRExxL4JAbAVBbZ7LXcvT7nvsP3bXunQJZ8gYF7kxLyM7NNZT00ty8Yfo7W"
+);
+
 const Routing = () => {
   return (
     <Router>
@@ -14,7 +22,14 @@ const Routing = () => {
         <Route>
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/payments" element={<Payment />} />
+          <Route
+            path="/payments"
+            element={
+              <Elements stripe={stripePromise}>
+                <Payment />
+              </Elements>
+            }
+          />
           <Route path="/orders" element={<Orders />} />
           <Route path="/category/:categoryName" element={<Results />} />
           <Route path="/products/:productId" element={<ProductDetail />} />
