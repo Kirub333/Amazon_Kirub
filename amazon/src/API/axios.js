2f2b1492933f2a5f81b91bd63813be265a5bd56c
Kirubel Ayele <kirub333@gmail.com>
2024-11-27 17:24:40 +0300

    Backend Deployed base url

--- amazon/src/API/axios.js
+++ amazon/src/API/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // backend locally using express server on port 5000
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  // deployed on render.com
+  baseURL: "https://amazon-kirub.onrender.com",
 });
 
 export { axiosInstance };
